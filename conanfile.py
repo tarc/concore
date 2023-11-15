a80d3c5d244e28b308177665e684de98bb1d5f3f
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2023-11-15 13:53:54 -0300

    Update recipe for Conan 2.0 compat. Update packages.
    The packages already required are updated to the closest supported
    version found in Conan Center.
    
    An option is added to not require TBB, mapping the `concore.no_tbb` CMake
    option. As only the new OneTBB has supported packages in Conan Center,
    the most recent OneTBB package is chosen:
    
    - onetbb/2021.10.0

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,10 @@
-from conans import ConanFile, CMake, tools
-import re
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
+from conan.tools.files import copy, load, collect_libs
+from conan.tools.microsoft.visual import is_msvc
 from os import path
+from re import search
+
 
 class ConcoreRecipe(ConanFile):
    name = "concore"
@@ -12,53 +16,73 @@ class ConcoreRecipe(ConanFile):
    license = "MIT"
 
    settings = "os", "compiler", "build_type", "arch"
-   generators = "cmake"
    build_policy = "missing"   # Some of the dependencies don't have builds for all our targets
 
-   options = {"shared": [True, False], "fPIC": [True, False]}
-   default_options = {"shared": False, "fPIC": True, "catch2:with_main": True}
+   options = {"shared": [True, False], "fPIC": [True, False], "no_tbb": [True, False]}
+   default_options = {"shared": False, "fPIC": True, "no_tbb": False, "catch2/*:with_main": True}
 
    exports = "LICENSE"
-   exports_sources = ("src/*", "include/*", "CMakeLists.txt")
+   exports_sources = ("src/*", "test/*", "CMakeLists.txt", "LICENSE")
 
    def set_version(self):
       # Get the version from src/CMakeList.txt project definition
-      content = tools.load(path.join(self.recipe_folder, "src/CMakeLists.txt"))
-      version = re.search(r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content).group(1)
+      content = load(self, path.join(self.recipe_folder, "src", "CMakeLists.txt"))
+      version = search(r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content).group(1)
       self.version = version.strip()
 
    @property
    def _run_tests(self):
-       return tools.get_env("CONAN_RUN_TESTS", False)
+      return not self.conf.get("tools.build:skip_test", default=True)
+   
+   @property
+   def _src_folder(self):
+      return "." if self._run_tests else "src"
 
    def build_requirements(self):
+      if not self.options.no_tbb:
+         self.test_requires("onetbb/[>=2021.10.0]")
+
       if self._run_tests:
-         self.build_requires("catch2/2.13.6")
-         self.build_requires("rapidcheck/20210107")
-         self.build_requires("benchmark/1.5.3")
+         self.test_requires("catch2/2.13.7")
+         self.test_requires("rapidcheck/cci.20210107")
+         self.test_requires("benchmark/1.5.6")
 
    def config_options(self):
-       if self.settings.os == "Windows":
-           del self.options.fPIC
+      if self.settings.os == "Windows":
+         del self.options.fPIC
+
+   def layout(self):
+      cmake_layout(self, src_folder = self._src_folder)
+
+   def generate(self):
+      tc = CMakeToolchain(self)
+      tc.variables["concore.no_tbb"] = self.options.no_tbb
+      if self.settings.os == "Windows" and is_msvc(self) and self.options.shared:
+         tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+      tc.generate()
+
+      deps = CMakeDeps(self)
+      deps.generate()
 
    def build(self):
       # Note: options "shared" and "fPIC" are automatically handled in CMake
       cmake = self._configure_cmake()
       cmake.build()
+      if self._run_tests:
+         cmake.test()
 
    def package(self):
-      self.copy(pattern="LICENSE", dst="licenses")
+      copy(self, "LICENSE", src=self.source_folder, dst=path.join(self.package_folder, "licenses"))
       cmake = self._configure_cmake()
       cmake.install()
 
    def package_info(self):
-      self.cpp_info.libs = self.collect_libs()
+      self.cpp_info.libs = collect_libs(self)
+
+   def package_id(self):
+      del self.info.options.no_tbb
 
    def _configure_cmake(self):
       cmake = CMake(self)
-      if self.settings.compiler == "Visual Studio" and self.options.shared:
-         cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
-      cmake.configure(source_folder=None if self._run_tests else "src")
+      cmake.configure()
       return cmake
-
-
